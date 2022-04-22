44fee568224926120d8842a4ac350e33bb75d88a
Namwon Kim <knw1234@gmail.com>
2022-04-22 16:16:01 +0900

    폰트 적용

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import '../styles/global.css'
+import '../styles/d2coding-subset.css'
 import {useRouter} from 'next/router';
 import CommonLayout from '../components/layout/CommonLayout';
 import AdminLayout from '../components/layout/AdminLayout';

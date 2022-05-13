8eb0e125d5fc7439cceddde3b19fd821a4d699c1
Namwon Kim <knw1234@gmail.com>
2022-05-13 10:35:44 +0900

    이미지가 나오도록 rewrite 기능 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const BACKEND_URL = {
+    BLOG_URL_DEV: 'http://localhost:9090',
+    BLOG_URL_PROD: 'http://blogback:8080',
+}
+
 module.exports = {
     env: {
-        BLOG_URL_DEV: 'http://localhost:9090',
-        BLOG_URL_PROD: 'http://blogback:8080',
+        BLOG_URL_DEV: BACKEND_URL.BLOG_URL_DEV,
+        BLOG_URL_PROD: BACKEND_URL.BLOG_URL_PROD,
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/file/:path',
+                destination: process.env.NODE_ENV === 'production' ?
+                    (BACKEND_URL.BLOG_URL_PROD + '/api/file/:path') :
+                    (BACKEND_URL.BLOG_URL_DEV + '/api/file/:path')
+            }
+        ]
+    }
 }

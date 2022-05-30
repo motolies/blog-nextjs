634ff50bb819c55b66bd1961aab3f9c8149c13de
Namwon Kim <knw1234@gmail.com>
2022-05-30 17:52:11 +0900

    태그 정보를 새로 가져온다.

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -1,5 +1,6 @@
 import PostComponent from "../../components/post/PostComponent"
 import service from "../../service"
+import {getAllTags} from "../../store/actions/tagActions"
 
 export default function PostPage({children, post, prevNext}) {
     return (
@@ -10,6 +11,10 @@ export default function PostPage({children, post, prevNext}) {
 }
 
 PostPage.getInitialProps = async (ctx) => {
+    // tag 정보를 새로 가져온다 
+    const store = ctx.store
+    await store.dispatch(getAllTags())
+
     const postId = ctx.query.id
     const postReq = await service.post.getPost({postId})
     const prevNextReq = await service.post.getPrevNext({postId})

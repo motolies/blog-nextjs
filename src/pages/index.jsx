0ec0d55cf65fed95b2922903dd4bebd8633e9080
motolies <knw1234@gmail.com>
2022-06-01 23:32:08 +0900

    즐겨찾기 부분 급하게 수정

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,82 @@ import {useEffect, useState} from "react"
 import service from "../service"
 import {SearchEngineComponent} from "../components/SearchEngineComponent"
 import {Box} from "@mui/material"
+import Link from "next/link"
+import LinkIcon from '@mui/icons-material/Link'
+import CategoryIcon from "@mui/icons-material/Category"
 
 export default function IndexPage({post}) {
     const [searchEngine, setSearchEngine] = useState([])
+
+    const favoriteSite = [
+        {
+            name: "Community",
+            links: [
+                {name: "clien", url: "http://www.clien.net/"},
+                {name: "okky", url: "https://okky.kr/"},
+                {name: "gorani", url: "https://gorani.kr/"},
+                {name: "AAGAG", url: "https://aagag.com/mirror/?target=_blank&time=12"},
+                {name: "뽐뿌", url: "http://www.ppomppu.co.kr/"},
+                {name: "뽐뿌 - 캠핑포럼", url: "http://m.ppomppu.co.kr/new/bbs_list.php?id=camping"},
+                {name: "뽐뿌 - 낚시포럼", url: "http://m.ppomppu.co.kr/new/bbs_list.php?id=fishing"},
+            ]
+        },
+        {
+            name: "Membership",
+            links: [
+                {name: "네이버플러스 X Lgu+", url: "https://nid.naver.com/membership/partner/uplus"},
+                {name: "네이버플러스", url: "https://nid.naver.com/membership/my?m=viewSaving"},
+            ]
+        },
+        {
+            name: "<devTools>",
+            links: [
+                {name: "Make readme.md", url: "https://www.makeareadme.com/"},
+                {name: "Code Sandbox", url: "https://codesandbox.io"},
+                {name: "Js Fiddle", url: "https://jsfiddle.net/user/fiddles/all/"},
+                {name: "Encoding", url: "https://coderstoolbox.net/"},
+                {name: "Json to JavaClass", url: "https://codebeautify.org/json-to-java-converter"},
+                {name: "Epoch & Unix Timestamp", url: "https://www.epochconverter.com/"},
+                {name: "regex101(정규식 검색)", url: "https://regex101.com/library"},
+                {name: "정규식 테스트", url: "https://regexr.com"},
+                {name: "정규식 테스트(.net)", url: "http://regexstorm.net/tester"},
+            ]
+        },
+        {
+            name: "WebTools",
+            links: [
+                {name: "웹용 포토샵", url: "https://www.photopea.com"},
+                {name: "Calc Cidr(IP대역계산)", url: "https://www.ipaddressguide.com/cidr"},
+            ]
+        },
+        {
+            name: "GeoService",
+            links: [
+                {name: "네이버 마이플레이스", url: "https://m.store.naver.com/myplace/home"},
+            ]
+        },
+        {
+            name: "etc",
+            links: [
+                {name: "VPN Gate", url: "http://www.vpngate.net/en/"},
+                {name: "Almost 2,400 DOS GAMES using PC Browsers", url: "https://archive.org/details/softwarelibrary_msdos_games/v2"},
+                {name: "Internet Arcade", url: "https://archive.org/details/internetarcade"},
+            ]
+        },
+        {
+            name: "철물점",
+            links: [
+                {name: "아이베란다", url: "http://www.iveranda.com/"},
+            ]
+        },
+        {
+            name: "Streaming",
+            links: [
+                {name: "누누.tv", url: "https://xn--od1ba.com/"},
+            ]
+        },
+    ]
+
     useEffect(() => {
         service.search.getAll().then(res => {
             console.log(res)
@@ -21,6 +94,35 @@ export default function IndexPage({post}) {
                 {/*TODO: 비어있는 박스를 계산해서 넣어주면 한 칸 당길 수 있다.*/}
             </Box>
             <hr/>
+            {favoriteSite.map((c) =>
+                <Box key={c.name}>
+                    <h3>{c.name}</h3>
+                    <ul className={'list'}>
+                        {c.links.map((c) =>
+                            <li key={c.name} className={'item'}>
+                                <a href={c.url} target="_blank" rel="noreferrer">
+                                    <Box sx={{display: 'inline-flex', alignItems: 'center'}}>
+                                        <LinkIcon/><span>{c.name}</span>
+                                    </Box>
+                                </a>
+                            </li>
+                        )}
+                    </ul>
+                </Box>
+            )}
+
+            <style jsx>
+                {`
+                  ul.list {
+                    margin: 0px;
+                  }
+
+                  li.item {
+                    display: inline-table;
+                    padding: .5rem 1rem .5rem .5rem;
+                  }
+                `}
+            </style>
         </>
     )
 }

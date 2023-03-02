eb0468b23065704edaaa89ccc651606e97df2046
Namwon Kim <knw1234@gmail.com>
2023-03-02 11:39:02 +0900

    index page search sync

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,9 @@
-import {useEffect, useState} from "react"
 import service from "../service"
 import {SearchEngineComponent} from "../components/SearchEngineComponent"
 import {Box} from "@mui/material"
-import Link from "next/link"
 import LinkIcon from '@mui/icons-material/Link'
-import CategoryIcon from "@mui/icons-material/Category"
 
-export default function IndexPage({post}) {
-    const [searchEngine, setSearchEngine] = useState([])
+export default function IndexPage({engines}) {
 
     const favoriteSite = [
         {
@@ -78,17 +74,10 @@ export default function IndexPage({post}) {
         },
     ]
 
-    useEffect(() => {
-        service.search.getAll().then(res => {
-            console.log(res)
-            setSearchEngine(res.data)
-        })
-    }, [])
-
     return (
         <>
             <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-                {searchEngine.map((s) =>
+                {engines.map((s) =>
                     <SearchEngineComponent key={s.id} name={s.name} url={s.url}/>
                 )}
                 {/*TODO: 비어있는 박스를 계산해서 넣어주면 한 칸 당길 수 있다.*/}
@@ -126,5 +115,10 @@ export default function IndexPage({post}) {
         </>
     )
 }
-
+IndexPage.getInitialProps = async (ctx) => {
+    const enginesReq = await service.search.getAll()
+    return {
+        engines: enginesReq.data
+    }
+}
 

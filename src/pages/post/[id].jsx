698a0910bf8413cdd12ab953da6fbc16fe8adf77
Namwon Kim <knw1234@gmail.com>
2023-03-03 15:13:57 +0900

    add og tags

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -3,17 +3,20 @@ import service from "../../service"
 import {getAllTags} from "../../store/actions/tagActions"
 import Head from "next/head";
 
-export default function PostPage({children, post, prevNext}) {
-
-    const description = post?.body?.replace(/<\/?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->/img, "").replace(/&nbsp;/img, "")
-    const tags = post?.tag?.map(tag => tag.name).join(', ')
+export default function PostPage({children, post, prevNext, meta}) {
 
     return (
         <>
             <Head>
-                <title>{post.subject}</title>
-                <meta name='description' content={description} />
-                <meta name="keyword" content={tags}/>
+                <title>{meta.title}</title>
+                <meta name='description' content={meta.description}/>
+                <meta name="keyword" content={meta.tags}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content={meta.page}/>
+                <meta property="og:title" content={meta.title}/>
+                <meta property="og:image" content="https://hvy.kr/images/logo.png"/>
+                <meta property="og:description" content={meta.description}/>
+                <meta property="og:site_name" content="Skyscape"/>
             </Head>
             <PostComponent post={post} prevNext={prevNext}/>
         </>
@@ -36,6 +39,12 @@ PostPage.getInitialProps = async (ctx) => {
 
     return {
         post: post.data,
-        prevNext: prevNext.data
+        prevNext: prevNext.data,
+        meta: {
+            title: post?.data?.subject,
+            description: post?.body?.replace(/<\/?[a-z][a-z0-9]*[^<>]*>|<!--.*?-->/img, "").replace(/&nbsp;/img, ""),
+            tags: post?.tag?.map(tag => tag.name).join(', '),
+            page: "https://" + ctx.req.headers.host + ctx.req.url
+        }
     }
 }

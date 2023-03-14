2ea4596a742fad77966d7288998ae68947ffac7a
Namwon Kim <knw1234@gmail.com>
2023-03-14 10:55:50 +0900

    add sitemap.xml.js

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,49 @@
+const API_HOST = process.env.NODE_ENV === 'development'
+    ? process.env.BLOG_URL_DEV
+    : process.env.BLOG_URL_PROD
+
+const HOST = 'https://hvy.kr';
+
+function generateSiteMap(posts) {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${HOST}</loc>
+     </url>
+     ${posts
+        .map((id) => {
+            return `
+       <url>
+           <loc>${`${HOST}/post/${id}`}</loc>
+       </url>
+     `;
+        })
+        .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+    // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({res}) {
+    // We make an API call to gather the URLs for our site
+    const request = await fetch(API_HOST + '/api/post/public-content');
+    const posts = await request.json();
+
+    // We generate the XML sitemap with the posts data
+    const sitemap = generateSiteMap(posts);
+
+    res.setHeader('Content-Type', 'text/xml');
+    // we send the XML to the browser
+    res.write(sitemap);
+    res.end();
+
+    return {
+        props: {},
+    };
+}
+
+export default SiteMap;

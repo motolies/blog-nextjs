036d254e7e863e9df60620cdcafbf37270450134
Namwon Kim <knw1234@gmail.com>
2024-04-22 22:43:35 +0900

    검색 수정

--- src/model/searchObject.js
+++ src/model/searchObject.js
@@ -1,13 +1,11 @@
 export const searchObjectInit = {
-    searchType: "TITLE",
-    searchCondition: {
-        keywords: [],
-        logic: "AND"
-    },
-    categories: [
-        {id: "ROOT", name: "전체글"}
-    ],
-    tags: [],
-    page: 1,
-    pageSize: 100
+  searchType: "TITLE",
+  searchCondition: {
+    keywords: [],
+    logic: "AND"
+  },
+  categories: [],
+  tags: [],
+  page: 1,
+  pageSize: 100
 }
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -9,81 +9,102 @@ import SearchFilter from "../components/search/SearchFilter"
 import {searchObjectInit} from "../model/searchObject"
 import {Pagination} from "@mui/lab"
 import {Stack} from "@mui/material"
+import {uuidV4Generator} from "../util/uuidUtil";
 
 export default function SearchPage({children}) {
-    const {enqueueSnackbar, closeSnackbar} = useSnackbar()
+  const {enqueueSnackbar, closeSnackbar} = useSnackbar()
 
-    const dispatch = useDispatch()
-    const router = useRouter()
+  const dispatch = useDispatch()
+  const router = useRouter()
 
-    const searchedPostState = useSelector((state) => state.post.searchedPost)
+  const searchedPostState = useSelector((state) => state.post.searchedPost)
 
-    const [searchAllParam, setSearchAllParam] = useState(searchObjectInit)
-    const [categories, setCategories] = useState(searchObjectInit.categories)
-    const [tags, setTags] = useState(searchObjectInit.tags)
-    const [keywords, setKeywords] = useState(searchObjectInit.searchCondition.keywords)
-    const [logic, setLogic] = useState(searchObjectInit.searchCondition.logic)
-    const [searchType, setSearchType] = useState(searchObjectInit.searchType)
-    const [page, setPage] = useState(searchObjectInit.page)
-    const [pageSize, setPageSize] = useState(searchObjectInit.pageSize)
+  const [searchAllParam, setSearchAllParam] = useState(searchObjectInit)
+  const [categories, setCategories] = useState(searchObjectInit.categories)
+  const [tags, setTags] = useState(searchObjectInit.tags)
+  const [keywords, setKeywords] = useState(searchObjectInit.searchCondition.keywords)
+  const [logic, setLogic] = useState(searchObjectInit.searchCondition.logic)
+  const [searchType, setSearchType] = useState(searchObjectInit.searchType)
+  const [page, setPage] = useState(searchObjectInit.page)
+  const [pageSize, setPageSize] = useState(searchObjectInit.pageSize)
 
+  useEffect(() => {
+    if (router.query?.q) {
+      // 디코딩을 한 다음에 각각 분배한다.
+      const decodeString = base64Decode(router.query.q)
+      const newObj = JSON.parse(decodeString)
+      newObj.searchCondition.keywords = newObj.searchCondition.keywords.filter(k => k.name.trim().length > 0)
 
-    useEffect(() => {
-        // 디코딩을 한 다음에 각각 분배한다.
-        const decodeString = base64Decode(router.query.q)
-        const newObj = JSON.parse(decodeString)
-        newObj.searchCondition.keywords = newObj.searchCondition.keywords.filter(k => k.name.trim().length > 0)
+      const newSearchAllParam = {...searchObjectInit, ...newObj}
+      setSearchAllParam(newSearchAllParam)
 
-        const newSearchAllParam = {...searchObjectInit, ...newObj}
-        setSearchAllParam(newSearchAllParam)
-
-        console.log("searchAllParam => ", searchAllParam)
-        dispatch(searchMultiple({searchAllParam: newSearchAllParam}))
-
-    }, [router.query.q])
-
-    useEffect(() => {
-        setCategories(searchAllParam.categories)
-        setTags(searchAllParam.tags)
-        setKeywords(searchAllParam.searchCondition.keywords)
-        setLogic(searchAllParam.searchCondition.logic)
-        setSearchType(searchAllParam.searchType)
-        setPage(searchAllParam.page)
-        setPageSize(searchAllParam.pageSize)
-
-    }, [searchAllParam])
-
-    const goPage = (event, newPage) => {
-        if (page !== newPage) {
-            const newSearchAllParam = {...searchAllParam, ...{page: newPage}}
-            router.push({pathname: '/search', query: {q: base64Encode(JSON.stringify(newSearchAllParam))}})
+      console.log("searchAllParam => ", searchAllParam)
+      dispatch(searchMultiple({searchAllParam: newSearchAllParam}))
+    } else if (router.query?.query) {
+      const newObj = {
+        searchCondition: {
+          keywords: [{
+            id: uuidV4Generator(),
+            name: router.query?.query
+          }],
+          logic: "AND"
         }
+      }
+      const newSearchAllParam = {...searchObjectInit, ...newObj}
+      setSearchAllParam(newSearchAllParam)
+
+      console.log("searchAllParam => ", searchAllParam)
+      dispatch(searchMultiple({searchAllParam: newSearchAllParam}))
     }
 
-    return (
-        <div>
-            <SearchFilter
-                defaultLogic={logic}
-                defaultSearchType={searchType}
-                defaultKeyword={keywords}
-                defaultCategories={categories}
-                defaultTags={tags}/>
-            <SearchResult/>
-            <Stack spacing={2} alignItems="center">
-                <Pagination
-                    count={searchedPostState.totalPage} page={page} onChange={goPage} color="primary"
-                    sx={{
-                        '& .Mui-selected': {
-                            backgroundColor: '#f0e3c1',
-                            color: 'white',
-                            opacity: 0.8
-                        }, "& .MuiPaginationItem-root": {
-                            color: "black",
-                            fontFamily: 'Montserrat',
-                        }
-                    }}
-                />
-            </Stack>
-        </div>
-    )
+  }, [router.query?.q, router.query?.query])
+
+  useEffect(() => {
+    setCategories(searchAllParam.categories)
+    setTags(searchAllParam.tags)
+    setKeywords(searchAllParam.searchCondition.keywords)
+    setLogic(searchAllParam.searchCondition.logic)
+    setSearchType(searchAllParam.searchType)
+    setPage(searchAllParam.page)
+    setPageSize(searchAllParam.pageSize)
+
+  }, [searchAllParam])
+
+  const goPage = (event, newPage) => {
+    if (page !== newPage) {
+      const newSearchAllParam = {...searchAllParam, ...{page: newPage}}
+      router.push({
+        pathname: '/search',
+        query: {q: base64Encode(JSON.stringify(newSearchAllParam))}
+      })
+    }
+  }
+
+  return (
+      <div>
+        <SearchFilter
+            defaultLogic={logic}
+            defaultSearchType={searchType}
+            defaultKeyword={keywords}
+            defaultCategories={categories}
+            defaultTags={tags}/>
+        <SearchResult/>
+        <Stack spacing={2} alignItems="center">
+          <Pagination
+              count={searchedPostState.totalPage} page={page} onChange={goPage}
+              color="primary"
+              sx={{
+                '& .Mui-selected': {
+                  backgroundColor: '#f0e3c1',
+                  color: 'white',
+                  opacity: 0.8
+                }, "& .MuiPaginationItem-root": {
+                  color: "black",
+                  fontFamily: 'Montserrat',
+                }
+              }}
+          />
+        </Stack>
+      </div>
+  )
 }

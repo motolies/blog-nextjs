6b382377efe3df17161b5d01388f405830f7a699
Namwon Kim <knw1234@gmail.com>
2025-10-11 23:17:27 +0900

    사용하지 않는 파일 삭제

--- src/components/layout/admin/WriteSection.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './WriteSection.module.css'
-
-export default function WriteSection({children, title, subtitle}) {
-    return (
-        <section className={styles.section}>
-            {children}
-        </section>
-    );
-}

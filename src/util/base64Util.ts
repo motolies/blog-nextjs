5cb71b9377cf7bdfae948758b7b046b82c9cdb7e
Namwon Kim <knw1234@gmail.com>
2026-06-13 13:02:19 +0900

    검색 버그 수정

--- src/util/base64Util.ts
+++ src/util/base64Util.ts
@@ -1,8 +1,16 @@
+// URL-safe base64(패딩 없음)로 인코딩한다. 표준 base64 후 +/- /_ 치환하고 = 패딩을 제거 (Java Base64.getUrlDecoder 호환)
 const base64Encode = (str: string): string => {
     return btoa(unescape(encodeURIComponent(str)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '')
 }
+// URL-safe 및 구 표준 base64 모두 디코딩한다. URL-safe 문자를 표준으로 되돌리고 누락된 패딩을 복원
 const base64Decode = (str: string): string => {
-    return decodeURIComponent(escape(atob(str)))
+    let s = str.replace(/-/g, '+').replace(/_/g, '/')
+    const pad = s.length % 4
+    if (pad) s += '='.repeat(4 - pad)
+    return decodeURIComponent(escape(atob(s)))
 }
 
 export {base64Encode, base64Decode}
